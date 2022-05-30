0acab94c2257662fc86b14388e1fb1b19282a083
Jean Carlos <jeangplay@gmail.com>
2022-05-29 21:25:06 -0300

    Visitantes ajustes

--- js/visitantes.js
+++ js/visitantes.js
@@ -1,3 +1,5 @@
+
+//Mascara para formulario de cadastro
 $(document).ready(function(){
     $('#telefone').mask('(00)00000-0000');
   });
@@ -17,20 +19,50 @@ async function func() {
 
     for (var i = 0; i < visitantes.length; i++) {
 
+        var nome = visitantes[i]["nome"];
+        var telefone = visitantes[i]["telefone"];
+        var email = visitantes[i]["email"];
+
         var newRow = tbodyRef.insertRow();
         //Nome
         var newCell = newRow.insertCell();
-        var nome = document.createTextNode(visitantes[i]["nome"]);
+        var nome = document.createTextNode(nome);
         newCell.appendChild(nome);
 
         //Telefone
         var newCell = newRow.insertCell();
-        var telefone = document.createTextNode(visitantes[i]["telefone"]);
+        var telefone = document.createTextNode(telefone);
         newCell.appendChild(telefone);
 
         //Email
         var newCell = newRow.insertCell();
-        var email = document.createTextNode(visitantes[i]["email"]);
+        var email = document.createTextNode(email);
         newCell.appendChild(email);
+
+        //Ações
+        var newCell = newRow.insertCell();
+
+        var editIcon = document.createElement("a");
+        editIcon.setAttribute('class', 'teste fa fa-pencil');
+        editIcon.setAttribute('editName', visitantes[i]["email"]);
+        newCell.appendChild(editIcon);
+
+        var deleteIcon = document.createElement("a");
+        deleteIcon.setAttribute('class', 'teste fas fa-trash');
+        deleteIcon.setAttribute('href', '#');
+        deleteIcon.setAttribute('excluirUser', visitantes[i]["email"]);
+        
+        newCell.appendChild(deleteIcon);
     }
 }
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    document.addEventListener('click', function(e){
+        if(e.target.tagName=="A"){
+            var image = e.target.getAttribute("excluirUser");
+            alert(image);
+        }
+      })
+ }, false);
+
