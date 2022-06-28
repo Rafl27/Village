d2cb6e8854e8e3bca2e6150f1cc63a0e790e5dcb
Rafl27 <raflsouza27@gmail.com>
2022-06-28 10:47:27 -0300

    pp

--- Front/js/profile.js
+++ Front/js/profile.js
@@ -7,7 +7,10 @@ let telefoneUsuario = JSON.parse(usuarioLogado).telefone;
 let emailUsuario = JSON.parse(usuarioLogado).email;
 let permissaoUsuario = JSON.parse(usuarioLogado).permissao;
 let ruaUsuario = JSON.parse(usuarioLogado).endereco;
-
+let idadeUsuario = JSON.parse(usuarioLogado).idade;
+let numeroUsuario = JSON.parse(usuarioLogado).numero;
+let cpfUsuario = JSON.parse(usuarioLogado).cpf;
+let imagemDePerfil = JSON.parse(usuarioLogado).pp;
 
 //mudando atributos
 document.getElementById('nomeUsuario').setAttribute('value',nomeUsuario);
@@ -15,3 +18,9 @@ document.getElementById('telefoneUsuario').setAttribute('value',telefoneUsuario)
 document.getElementById('emailUsuario').setAttribute('value',emailUsuario);
 document.getElementById('permissaoUsuario').setAttribute('value',permissaoUsuario);
 document.getElementById('ruaUsuario').setAttribute('value',ruaUsuario);
+document.getElementById('idadeUsuario').setAttribute('value',idadeUsuario);
+document.getElementById('numeroUsuario').setAttribute('value',numeroUsuario);
+document.getElementById('cpfUsuario').setAttribute('value',cpfUsuario);
+document.getElementById('nomeAbaixoDaPP').innerHTML = nomeUsuario;
+document.getElementById('emailAbaixoDaPP').innerHTML = emailUsuario;
+document.getElementById('ppUsuario').src = imagemDePerfil;
